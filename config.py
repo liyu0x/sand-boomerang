560922fb7499f4e217c0527ed5aa919f5d81e04b
0xliyu <liyu0x@outlook.com>
2024-03-17 14:52:38 +0800

    init

---
+++ config.py
@@ -0,0 +1,26 @@
+# Paths to the STP and cryptominisat executable
+
+# LINUX
+# PATH_STP = "../stp/build/stp"
+# PATH_CRYPTOMINISAT = "../cryptominisat/build/cryptominisat5"
+# PATH_SHARP = "../sharpsat-td/bin/sharpSAT"
+# PATH_BOOLECTOR = "/opt/homebrew/bin/boolector"
+
+# MACOS
+# PATH_STP = "/opt/homebrew/bin/stp"
+# PATH_CRYPTOMINISAT = "/opt/homebrew/bin/cryptominisat5"
+# PATH_SHARP = "../sharpsat-td/bin/sharpSAT"
+# PATH_BOOLECTOR = "/opt/homebrew/bin/boolector"
+
+# Windows
+PATH_STP = "../outer/stp"
+PATH_CRYPTOMINISAT = "../outer/cryptominisat5"
+PATH_SHARP = "../sharpsat-td/bin/sharpSAT"
+PATH_BOOLECTOR = "/opt/homebrew/bin/boolector"
+
+# Maximum weight for characteristics to search for
+MAX_WEIGHT = 1000
+# Maximum number of characteristics to search for a differential
+MAX_CHARACTERISTICS = 10000000
+MULTI_THREADS = 6
+USE_SHARP = 0
